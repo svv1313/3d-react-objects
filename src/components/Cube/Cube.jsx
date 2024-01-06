30e78a0803e6605e59dd7dec01f27d25f15fcc01
svv1313 <seleznev19963@gmail.com>
2024-01-06 21:31:59 +0100

    feat: added frame animation

--- src/components/Cube/Cube.jsx
+++ src/components/Cube/Cube.jsx
@@ -1,9 +1,39 @@
+import { useState, useRef } from "react";
+import { useFrame } from "react-three-fiber";
 
 const Cube = (props) => {
+  const [isBig, setIsBig] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+  const ref = useRef();
+
+  useFrame(() => {
+    ref.current.rotation.x += 0.01;
+    ref.current.rotation.y += 0.01;
+  });
+
+  const onMeshClick = () => {
+    setIsBig(!isBig);
+  };
+
+  const onMeshHover = (value) => {
+    return () => {
+      setIsHovered(value);
+    };
+  };
+
+  const size = isBig ? 2 : 1;
+  const color = isHovered ? "green" : "blue";
+
   return (
-    <mesh {...props}>
-      <boxGeometry attach="geometry" args={[1, 1, 1]} />
-      <meshStandardMaterial attach="material" color="blue" />
+    <mesh
+      {...props}
+      ref={ref}
+      onClick={onMeshClick}
+      onPointerOver={onMeshHover(true)}
+      onPointerOut={onMeshHover(false)}
+    >
+      <boxGeometry attach="geometry" args={[size, size, size]} />
+      <meshStandardMaterial attach="material" color={color} />
     </mesh>
   );
 };
